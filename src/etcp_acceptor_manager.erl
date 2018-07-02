97a0023dbae4a02b550dacecf78b600bdedbffcc
pouriya <pouriya.jahanbakhsh@gmail.com>
2018-07-02 20:21:04 +0430

    refactor: clean up new API

--- src/etcp_acceptor_manager.erl
+++ src/etcp_acceptor_manager.erl
@@ -1,170 +1,218 @@
-%%%-------------------------------------------------------------------
-%%% @author pouriya
-%%% @copyright (C) 2018, <COMPANY>
-%%% @doc
+%%% ------------------------------------------------------------------------------------------------
+%%% ETCP is available for use under the following license, commonly known as the 3-clause (or
+%%% "modified") BSD license:
 %%%
-%%% @end
-%%% Created : 09. Apr 2018 10:47 PM
-%%%-------------------------------------------------------------------
--module(etcp_acceptor).
--author("pouriya").
+%%% Copyright (c) 2017-2018, Pouriya Jahanbakhsh
+%%% (pouriya.jahanbakhsh@gmail.com)
+%%% All rights reserved.
+%%%
+%%% Redistribution and use in source and binary forms, with or without modification, are permitted
+%%% provided that the following conditions are met:
+%%%
+%%% 1. Redistributions of source code must retain the above copyright notice, this list of
+%%%    conditions and the following disclaimer.
+%%%
+%%% 2. Redistributions in binary form must reproduce the above copyright notice, this list of
+%%%    conditions and the following disclaimer in the documentation and/or other materials provided
+%%%    with the distribution.
+%%%
+%%% 3. Neither the name of the copyright holder nor the names of its contributors may be used to
+%%%    endorse or promote products derived from this software without specific prior written
+%%%    permission.
+%%%
+%%% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+%%% IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+%%% FITNESS FOR A  PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+%%% CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+%%% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+%%% SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+%%% THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+%%% OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+%%% POSSIBILITY OF SUCH DAMAGE.
+%%% ------------------------------------------------------------------------------------------------
+%% @author  Pouriya Jahanbakhsh <pouriya.jahanbakhsh@gmail.com>
+%% @version 17.9.10
+%% -------------------------------------------------------------------------------------------------
+-module(etcp_acceptor_manager).
+-author("pouriya.jahanbakhsh@gmail.com").
+%% -------------------------------------------------------------------------------------------------
+%% Exports:
 
 -export([start_link/5
+        ,start_link/4
         ,mode/1
-        ,sleep/1
-        ,accept/1
-        ,accept/3]).
+        ,suspend/1
+        ,resume/1
+        ,filter_mode/1
+        ,terminate/3]).
 
--export([init/6
-        ,loop/3
-        ,handle_debug/3]).
+%% 'proc_lib' callback:
+-export([init/6]).
 
-%% API
--export([]).
--include("internal/etcp_guard.hrl").
--include("internal/etcp_acceptor.hrl").
--define(DEF_DEBUG, []).
--define(DEF_TRANSPORT_MODULE, 'etcp_transporter_tcp').
--define(DEF_START_OPTIONS, []).
--define(DEF_TRANSPORT_OPTIONS, []).
--define(GEN_CALL_TAG, '$gen_call').
+%% Internal export:
+-export([loop/3]).
+
+%% 'sys' callbacks:
+-export([handle_debug/3
+        ,system_terminate/4
+        ,system_replace_state/2
+        ,system_get_state/1
+        ,system_continue/3
+        ,system_code_change/4]).
+
+%% -------------------------------------------------------------------------------------------------
+%% Records & Macros & Includes:
+
 -define(S, state).
--record(?S, {module
-            ,init_argument
-            ,name
-            ,options
-            ,mode
-            ,process_registry
-            ,listen_socket
-            ,transporter_state
-            ,transporter
-            ,rate_limit}).
+-record(?S, {mode, acceptor}).
+
+-include("internal/etcp_acceptor.hrl").
+-define(GEN_CALL_TAG, '$gen_call').
+-define(HIBERNATE_TIMEOUT, 10000).
+-define(MODE_TAG, 'mode').
+-define(RESUME_TAG, 'resume').
+-define(SUSPEND_TAG, 'suspend').
+-define(NO_PROCESS_KEEPER, 'undefined').
+
+%% -------------------------------------------------------------------------------------------------
+%% API:
 
 -spec
-start_link(module(), term(), etcp_types:start_options(), etcp_types:socket(), pid()) ->
-    etcp_types:start_return().
-start_link(Mod, InitArg, Opts, LSock, ProcReg) when erlang:is_atom(Mod) andalso
-                                                    erlang:is_map(Opts) andalso
-                                                    ?is_process_registry(ProcReg) ->
-    proc_lib:start_link(?MODULE, init, [erlang:self(), Mod, InitArg, Opts, LSock, ProcReg]).
+start_link(module(), term(), etcp:start_options(), etcp:socket(), etcp:process_registry()) ->
+    etcp:start_return().
+start_link(Mod, InitArg, Opts, TrS, ProcKeeper) when erlang:is_atom(Mod) andalso
+                                                     erlang:is_map(Opts)      ->
+    proc_lib:start_link(?MODULE, init, [erlang:self(), Mod, InitArg, Opts, TrS, ProcKeeper]).
 
 
-mode(Acceptor) when erlang:is_pid(Acceptor) ->
-    gen_server:call(Acceptor, mode).
+-spec
+start_link(module(), term(), etcp:start_options(), etcp:socket()) ->
+    etcp:start_return().
+start_link(Mod, InitArg, Opts, TrS) when erlang:is_atom(Mod) andalso
+                                         erlang:is_map(Opts)      ->
+    proc_lib:start_link(?MODULE
+                       ,init
+                       ,[erlang:self(), Mod, InitArg, Opts, TrS, ?NO_PROCESS_KEEPER]).
 
 
-sleep(Acceptor) when erlang:is_pid(Acceptor) ->
-    gen_server:call(Acceptor, ?ACCEPTOR_SLEEP_MODE).
+mode(AccMngr) when erlang:is_pid(AccMngr) ->
+    gen_server:call(AccMngr, ?MODE_TAG).
 
 
-accept(Acceptor) when erlang:is_pid(Acceptor) ->
-    gen_server:call(Acceptor, ?ACCEPTOR_ACCEPT_MODE).
+suspend(AccMngr) when erlang:is_pid(AccMngr) ->
+    gen_server:call(AccMngr, ?SUSPEND_TAG).
 
 
-init(Parent, Mod, InitArg, Opts, LSock, ProcReg) ->
-    TrMod = etcp_utils:get_value(transporter, Opts, ?DEF_TRANSPORT_MODULE, fun erlang:is_atom/1),
-    TrOpts = etcp_utils:get_value(transporter_options, Opts, ?DEF_TRANSPORT_OPTIONS, fun(_) -> true end),
-    case etcp_transporter:init(TrMod, TrOpts) of
-        {ok, TrState} ->
-            DbgOpts = etcp_utils:get_value(acceptor_debug, Opts, ?DEF_DEBUG, fun erlang:is_list/1),
-            Mode = etcp_utils:get_value(acceptor_mode, Opts, ?DEF_ACCEPTOR_MODE, fun filter_mode/1),
+resume(AccMngr) when erlang:is_pid(AccMngr) ->
+    gen_server:call(AccMngr, ?RESUME_TAG).
+
+%% -------------------------------------------------------------------------------------------------
+%% 'proc_lib' callback:
+
+%% @hidden
+init(Parent, Mod, InitArg, Opts, TrState, ProcKeeper) ->
+    Mode = etcp_utils:get_value(acceptor_mode, Opts, ?DEF_ACCEPTOR_MODE, {?MODULE, filter_mode, 1}),
+    erlang:process_flag(trap_exit, true),
+    Args =
+        if
+            ProcKeeper == ?NO_PROCESS_KEEPER ->
+                [Mod, InitArg, Opts, TrState, Mode];
+            true ->
+                [Mod, InitArg, Opts, TrState, Mode, ProcKeeper]
+        end,
+    try erlang:apply(etcp_acceptor, start_link, Args) of
+        {ok, Pid} ->
             proc_lib:init_ack(Parent, {ok, erlang:self()}),
-            Name = erlang:self(),
-            Dbg = etcp_utils:debug_options(?MODULE, Name, DbgOpts),
-            Dbg2 = debug(Name, Dbg, {start, TrMod, LSock, ProcReg, Mode}),
-            State = #?S{module = Mod
-                       ,init_argument = InitArg
-                       ,name = Name
-                       ,options = Opts
-                       ,mode = Mode
-                       ,process_registry = ProcReg
-                       ,transporter_state = TrState
-                       ,transporter = TrMod
-                       ,listen_socket = LSock},
-            loop(Parent, Dbg2, State);
+            erlang:garbage_collect(),
+            loop(Parent, [], #?S{mode = Mode, acceptor = Pid});
         {error, Rsn}=Err ->
             proc_lib:init_ack(Parent, Err),
             erlang:exit(Rsn)
+    catch
+        _:Rsn -> % system_limit
+            Rsn2 = {start, [{reason, Rsn}, {module, etcp_acceptor}]},
+            proc_lib:init_ack(Parent, {error, Rsn2}),
+            erlang:exit(Rsn2)
     end.
 
+%% -------------------------------------------------------------------------------------------------
+%% 'sys' callbacks:
 
-loop(Parent
-    ,Dbg
-    ,#?S{mode = ?ACCEPTOR_ACCEPT_MODE
-        ,transporter = TrMod
-        ,listen_socket = LSock
-        ,transporter_state = TrState
-        ,options = Opts
-        ,name = Name
-        ,module = Mod
-        ,init_argument = InitArg
-        ,process_registry = ProcReg}=State) ->
-    try etcp_connection:start_link(Mod, InitArg, Opts, TrMod, LSock, TrState) of
-        {ok, Pid, TrState2} ->
-            Dbg2 = debug(Name, Dbg, {start_connection, Pid}),
-            Dbg3 =
-                if
-                    erlang:is_pid(ProcReg) ->
-                        etcp_server_process_registry:new(ProcReg, Pid),
-                        debug(Name, Dbg2, {process_registry, Pid, ProcReg});
-                    true ->
-                        Dbg2
-                end,
-            process_message(Parent, Dbg3, State#?S{transporter_state = TrState2});
-        {error, {socket_accept, [{reason, timeout}|_]}} ->
-            process_message(Parent, Dbg, State);
-        {error, {socket_accept, [{reason, emfile}|_]}} ->
-            error_logger:warning_msg("ETCP acceptor ~p: too many alive connections", [Name]),
-            timer:sleep(100),
-            process_message(Parent, Dbg, State);
-        {error, Rsn} ->
-            terminate(Dbg, State, Rsn)
-    catch
-        _:Rsn ->
-            error_logger:warning_msg("ETCP acceptor ~p: could not start new process for reason ~p"
-                                    ,[Name, Rsn]),
-            timer:sleep(100),
-            process_message(Parent, Dbg, State)
-    end;
-loop(Parent, Dbg, State) ->
-    process_message(Parent, Dbg, State).
+%% @hidden
+system_code_change(S, _Module, _OldVsn, _Extra) ->
+    {ok, S}.
 
 
-process_message(Parent, Dbg, #?S{name = Name}=State) ->
+%% @hidden
+system_continue(Parent, Dbg, State) ->
+    ?MODULE:loop(Parent, Dbg, State).
+
+
+%% @hidden
+system_get_state(S) ->
+    {ok, S}.
+
+
+%% @hidden
+system_replace_state(StateFun, S) ->
+    S2 = StateFun(S),
+    {ok, S2, S2}.
+
+
+%% @hidden
+system_terminate(Rsn, _, Dbg, State) ->
+    terminate(Dbg, State, Rsn).
+
+
+handle_debug(IODev, Event, Name) ->
+    io:format(IODev, "*DBG* ETCP acceptor ~p got debug event ~p\n", [Name, Event]).
+
+%% -------------------------------------------------------------------------------------------------
+%% Internals:
+
+%% @hidden
+loop(Parent, Dbg, S) ->
     receive
         Msg ->
-            process_message(Parent, debug(Name, Dbg, {in, Msg}), State, Msg)
-    after 0 ->
-        ?MODULE:loop(Parent, Dbg, State)
+            process_message(Msg, Parent, Dbg, S)
+    after ?HIBERNATE_TIMEOUT ->
+        erlang:hibernate(?MODULE, loop, [Parent, Dbg, S])
     end.
 
 
-process_message(Parent, Dbg, #?S{name = Name}=State, {?GEN_CALL_TAG, From, Request}) ->
-    {Dbg2, State2} = process_request(debug(Name, Dbg, {call, From, Request}), State, From, Request),
-    ?MODULE:loop(Parent, Dbg2, State2).
+process_message({?GEN_CALL_TAG, From, Request}, Parent, Dbg, State) ->
+    {Dbg2, State2} = process_request(Request, debug(Dbg, {call, From, Request}), State, From),
+    ?MODULE:loop(Parent, Dbg2, State2);
+process_message({'EXIT', Pid, Rsn}, _, Dbg, State) when Pid == State#?S.acceptor ->
+    terminate(Dbg, State, Rsn);
+process_message({'EXIT', Pid, Rsn}, Parent, Dbg, State) when Pid == Parent ->
+    terminate(Dbg, State, Rsn);
+process_message({system, From, SysMsg}, Parent, Dbg, State) ->
+    sys:handle_system_msg(SysMsg, From, Parent, ?MODULE, Dbg, State);
+process_message(_, Parent, Dbg, State) ->
+    ?MODULE:loop(Parent, Dbg, State).
 
 
-process_request(Dbg, #?S{name = Name, mode = Mode}=State, From, mode) ->
-    {reply(Name, Dbg, From, Mode), State};
-process_request(Dbg, #?S{name = Name}=State, From, Mode) when Mode =:= ?ACCEPTOR_ACCEPT_MODE orelse
-                                                              Mode =:= ?ACCEPTOR_SLEEP_MODE ->
-    {reply(Name, Dbg, From, Mode), State#?S{mode = Mode}};
-process_request(Dbg, #?S{name = Name}=State, From, Request) ->
-    {reply(Name, Dbg, From, {error, {unknown, [{request, Request}]}}), State}.
+process_request(?MODE_TAG, Dbg, State, From) ->
+    {reply(Dbg, From, State#?S.mode), State};
+process_request(Action, Dbg, State, From) when Action == ?SUSPEND_TAG ->
+    etcp_acceptor:suspend(State#?S.acceptor),
+    {reply(Dbg, From, ok), State#?S{mode = ?ACCEPTOR_SLEEP_MODE}};
+process_request(Action, Dbg, State, From) when Action == ?RESUME_TAG ->
+    etcp_acceptor:resume(State#?S.acceptor),
+    {reply(Dbg, From, ok), State#?S{mode = ?ACCEPTOR_ACCEPT_MODE}};
+process_request(Request, Dbg, State, From) ->
+    {reply(Dbg, From, {error, {unknown, [{request, Request}]}}), State}.
 
 
 terminate(_, _, Rsn) ->
     erlang:exit(Rsn).
 
 
-debug(_, [], _) ->
+debug([], _) ->
     [];
-debug(Name, Dbg, Event) ->
-    sys:handle_debug(Dbg, fun ?MODULE:handle_debug/3, Name, Event).
-
-
-handle_debug(IODev, Event, Name) ->
-    io:format(IODev, "*DBG* ETCP acceptor ~p got debug event ~p\n", [Name, Event]).
+debug(Dbg, Event) ->
+    sys:handle_debug(Dbg, fun ?MODULE:handle_debug/3, erlang:self(), Event).
 
 
 filter_mode(?ACCEPTOR_ACCEPT_MODE) ->
@@ -175,13 +223,9 @@ filter_mode(_) ->
     false.
 
 
-reply(Name, Dbg, {Pid, Tag}=Client, Msg) ->
+reply(Dbg, {Pid, Tag}=Client, Msg) ->
     catch Pid ! {Tag, Msg},
-    debug(Name, Dbg, {out, Client, Msg});
-reply(Name, Dbg, Pid, Msg) ->
+    debug(Dbg, {out, Client, Msg});
+reply(Dbg, Pid, Msg) ->
     catch Pid ! Msg,
-    debug(Name, Dbg, {out, Pid, Msg}).
-
-
-accept(TrMod, LSock, TrState) ->
-    etcp_transporter:accept(TrMod, LSock, TrState).
+    debug(Dbg, {out, Pid, Msg}).
